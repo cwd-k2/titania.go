07de884fef52640e47dcdcc48e7dd69a83e2cab4
cwd-k2 <kaito.augmented7th@gmail.com>
2020-08-16 03:27:54 +0900

    pretty: 修正，太字と下線追加

--- pretty/main.go
+++ pretty/main.go
@@ -7,19 +7,27 @@ func Up(n int) {
 }
 
 func Down(n int) {
-	fmt.Printf("\033[%dA", n)
+	fmt.Printf("\033[%dB", n)
 }
 
 func Left(n int) {
-	fmt.Printf("\033[%dA", n)
+	fmt.Printf("\033[%dC", n)
 }
 
 func Right(n int) {
-	fmt.Printf("\033[%dA", n)
+	fmt.Printf("\033[%dD", n)
 }
 
 func Erase() {
 	fmt.Printf("\033[2K\033[G")
+}
+
+func Bold(str string) string {
+	return fmt.Sprintf("\033[1m%s\033[0m", str)
+}
+
+func UnderLine(str string) string {
+	return fmt.Sprintf("\033[4m%s\033[0m", str)
 }
 
 func Black(str string) string {
@@ -27,29 +35,29 @@ func Black(str string) string {
 }
 
 func Red(str string) string {
-	return fmt.Sprintf("\033[30m%s\033[39m", str)
+	return fmt.Sprintf("\033[31m%s\033[39m", str)
 }
 
 func Green(str string) string {
-	return fmt.Sprintf("\033[30m%s\033[39m", str)
+	return fmt.Sprintf("\033[32m%s\033[39m", str)
 }
 
 func Yellow(str string) string {
-	return fmt.Sprintf("\033[30m%s\033[39m", str)
+	return fmt.Sprintf("\033[33m%s\033[39m", str)
 }
 
 func Blue(str string) string {
-	return fmt.Sprintf("\033[30m%s\033[39m", str)
+	return fmt.Sprintf("\033[34m%s\033[39m", str)
 }
 
 func Magenta(str string) string {
-	return fmt.Sprintf("\033[30m%s\033[39m", str)
+	return fmt.Sprintf("\033[35m%s\033[39m", str)
 }
 
 func Cyan(str string) string {
-	return fmt.Sprintf("\033[30m%s\033[39m", str)
+	return fmt.Sprintf("\033[36m%s\033[39m", str)
 }
 
 func White(str string) string {
-	return fmt.Sprintf("\033[30m%s\033[39m", str)
+	return fmt.Sprintf("\033[37m%s\033[39m", str)
 }
