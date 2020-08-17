1990c1c4d479f289ccbe7d066a582454f53aab45
cwd-k2 <kaito.augmented7th@gmail.com>
2020-08-17 13:09:12 +0900

    [option] 引数の処理を追加 相対パスを綺麗に扱う

--- option.go
+++ option.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 // オプション解析
 func OptParse() ([]string, []string) {
 	// テストする言語を指定する
+	// ここは flag を使わずに自前処理でも良さそう
 	var flagLanguages string
 	flag.StringVar(
 		&flagLanguages,
@@ -20,12 +22,12 @@ func OptParse() ([]string, []string) {
 
 	var directories []string
 
-	if len(args) == 0 {
-		pwd, err := os.Getwd()
-		if err != nil {
-			panic(err)
-		}
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 
+	if len(args) == 0 {
 		entries, err := ioutil.ReadDir(pwd)
 		if err != nil {
 			panic(err)
@@ -38,7 +40,14 @@ func OptParse() ([]string, []string) {
 		}
 
 	} else {
-		directories = args
+		for _, directory := range args {
+			// パスを相対パスとして綺麗な形に
+			dirname, err := filepath.Rel(pwd, filepath.Join(pwd, directory))
+			if err != nil {
+				panic(err)
+			}
+			directories = append(directories, dirname)
+		}
 	}
 
 	var languages []string
